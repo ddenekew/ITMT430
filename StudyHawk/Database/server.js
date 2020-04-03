aa5cedf4de3632bdc715f0a4e278def81937744c
Pablo Villagomez <pvillag1@hawk.iit.edu>
2020-04-02 22:10:49 -0500

    setting up connections for DB

---
+++ StudyHawk/Database/server.js
@@ -0,0 +1,19 @@
+// load environment variables
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', (error) => console.log('Connected to database..'))
+
+// accept json post, get requests 
+app.use(express.json())
+
+const userRouter = require('./routes/users')
+app.use('/users', userRouter)
+
+app.listen(3000, () => console.log('Server started..'))
